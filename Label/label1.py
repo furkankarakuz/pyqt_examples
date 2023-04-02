b3fa41a35461a330e0cce8520532260802fe05de
furkankarakuz <karakuzfurkan.98@gmail.com>
2023-04-02 19:29:38 +0300

    add label widget test

---
+++ Label/label1.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+import sys
+
+def Main():
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.setWindowTitle("PyQt5 Label Test")
+    label = QLabel("Hello World",window)
+    window.resize(300,300)
+    window.show()
+    sys.exit(app.exec())
+Main()
---
+++ Window/window1.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+import sys
+
+def Main():
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.setWindowTitle("PyQt5 Window")
+    window.show()
+    sys.exit(app.exec())
+
+Main()
