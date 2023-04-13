7a27a88ecaf8e6896212254f5630bccbabf5b466
furkankarakuz <karakuzfurkan.98@gmail.com>
2023-04-13 23:55:30 +0300

    PyQt5 Document Examples

---
+++ Document/document1.py
@@ -0,0 +1,59 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+import sys
+
+class MainWindow(QMainWindow):
+    count = 0
+    def __init__(self,parent=None):
+        super(MainWindow,self).__init__(parent)
+
+        self.mdi = QMdiArea()
+        self.setCentralWidget(self.mdi)
+
+        bar = self.menuBar()
+
+        file = bar.addMenu("File")
+        file.addAction("New")
+        file.addAction("Cascade")
+        file.addAction("Tailed")
+
+        file.triggered.connect(self.windowAction)
+        self.setWindowTitle("MDI Demo")
+
+    
+    def windowAction(self,q):
+        print("Triggered")
+
+        if q.text()=="New":
+            MainWindow.count = MainWindow.count+1
+        
+            sub = QMdiSubWindow()
+            
+            widget = QWidget()
+            
+            v_box = QVBoxLayout()
+
+            button = QPushButton("Button")
+            
+            v_box.addWidget(button)
+            widget.setLayout(v_box)
+
+            sub.setWidget(widget)
+            sub.setWindowTitle("Test")
+            self.mdi.addSubWindow(sub)
+
+            sub.show()
+        
+        if q.text()=="Cascade":
+            self.mdi.cascadeSubWindows()
+        
+        if q.text()=="Tailed":
+            self.mdi.tileSubWindows()
+
+    
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec())
---
+++ Document/document2.py
@@ -0,0 +1,52 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self,parent=None):
+        super(MainWindow,self).__init__(parent)
+
+        self.count = 1
+
+        self.mdi = QMdiArea()
+        self.setCentralWidget(self.mdi)
+
+        bar = QMenuBar()
+        self.setMenuBar(bar)
+
+        menu=bar.addMenu("File")
+        menu.addAction(QAction("Add Tab",self))
+        menu.addAction("Cascade")
+        menu.addAction("Edit")
+
+        menu.triggered.connect(self.addTab)
+        self.show()
+    
+    def addTab(self,q):
+        if q.text()=="Add Tab":
+            sub = QMdiSubWindow()
+            self.setWindowTitle("{}.form".format(self.count))
+            self.count+=1
+
+            widget = QWidget()
+
+            v_box = QVBoxLayout()
+
+            v_box.addWidget(QTextEdit())
+            v_box.addWidget(QPushButton("Send"))
+            
+            widget.setLayout(v_box)
+            sub.setWidget(widget)
+
+            self.mdi.addSubWindow(sub)
+            sub.show()
+        elif q.text()=="Cascade":
+            self.mdi.cascadeSubWindows()
+        else:
+            self.mdi.tileSubWindows()
+
+app = QApplication(sys.argv)
+window = MainWindow()
+sys.exit(app.exec())
