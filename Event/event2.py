7d3876bf8ebe5cd5ea40600fa6663f4b359add4b
furkankarakuz <karakuzfurkan.98@gmail.com>
2023-04-15 22:04:24 +0300

    PyQt5 Event Examples

---
+++ Event/event2.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtGui import *
+
+import sys,os
+
+icons = "icons/"
+class DragRelease(QDialog):
+    def __init__(self,parent=None):
+        super(DragRelease,self).__init__(parent)
+
+        source_list = QListWidget()
+        for i in os.listdir(icons):
+            list_item = QListWidgetItem(i)
+            list_item.setIcon(QIcon((icons+i)))
+            source_list.addItem(list_item)
+
+        
+        source_list.setDragEnabled(True)
+
+        target_list = QListWidget()
+        target_list.setAcceptDrops(True)
+        target_list.setViewMode(QListView.IconMode)
+
+        h_box = QHBoxLayout()
+        h_box.addWidget(source_list)
+        h_box.addWidget(target_list)
+        self.setLayout(h_box)
+
+app = QApplication(sys.argv)
+window = DragRelease()
+window.show()
+sys.exit(app.exec())
